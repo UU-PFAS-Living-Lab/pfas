63f97079741176d5e9b1d9f3197cc13ddf65d04c
chStaiger <staigerchristine@gmail.com>
2025-10-23 13:29:20 +0200

    Update analytical_soln.py

--- src/pfas_leach_screening/analytical_soln.py
+++ src/pfas_leach_screening/analytical_soln.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.special import iv
 from scipy.special import erfc
 from pfas_leach_screening import utils
+from pfas_leach_screening.solvers import kinetic_solver
 
 def analytical_soln(t,z,t0,pfas_tot,Ci,L,v,theta,rhob,D,Kd,alphas,Fs,R,Rs,Raw,spaflag,cflag):
     # Compute the aqueous concentration (C1), solid-phase adsorption in the kinetic 
@@ -94,21 +95,22 @@ def analytical_soln(t,z,t0,pfas_tot,Ci,L,v,theta,rhob,D,Kd,alphas,Fs,R,Rs,Raw,sp
     
     # SPA is equilibrium
     if spaflag == 0:
+        C1_bvp, C1_ivp = kinetic_solver(R, Z, T, P, T0, C10, Ci)
         # Solution for the boundary value problem
         # Define the solution for a constant boundary condition as a function
-        eqbvpfunc = lambda T: 0.5 * erfc((R*Z-T)/(2*(T*R/P)**(1/2))) + ((T*P)/(np.pi*R))**(1/2) * np.exp(-(R*Z-T)**2/(4*T*R/P)) - 1/2 * (1 + P*Z + P*T/R) * np.exp(P*Z) * erfc((R*Z + T)/(2*(T*R/P)**(1/2)))
-        for i in range(len(T)):
-            if T[i] <= T0:
-                C1_bvp[:,i] = C10 * eqbvpfunc(T[i])
-            else:
-                C1_bvp[:,i] = C10 * eqbvpfunc(T[i]) - C10 * eqbvpfunc(T[i]-T0)
-        if max(Ci) != 0:
-            # Solution for the initial value problem
-            for i in range(len(T)):
-                for j in range(len(Z)):
-                    kesi = np.linspace(0,1,len(Ci))       
-                    eqivpfunc = lambda Z, T: (np.exp(-(R*Z-R*kesi-T)**2/(4*T*R/P)) + np.exp(-P*kesi - (R*Z+R*kesi-T)**2/(4*T*R/P)))/(2*np.sqrt(np.pi*T/P/R)) - P/2*np.exp(P*Z)*erfc((R*Z+R*kesi+T)/(2*np.sqrt(T*R/P)))            
-                    C1_ivp[j,i] = np.trapz(eqivpfunc(Z[j],T[i])*Ci,kesi)
+        #eqbvpfunc = lambda T: 0.5 * erfc((R*Z-T)/(2*(T*R/P)**(1/2))) + ((T*P)/(np.pi*R))**(1/2) * np.exp(-(R*Z-T)**2/(4*T*R/P)) - 1/2 * (1 + P*Z + P*T/R) * np.exp(P*Z) * erfc((R*Z + T)/(2*(T*R/P)**(1/2)))
+        #for i in range(len(T)):
+        #    if T[i] <= T0:
+        #        C1_bvp[:,i] = C10 * eqbvpfunc(T[i])
+        #    else:
+        #        C1_bvp[:,i] = C10 * eqbvpfunc(T[i]) - C10 * eqbvpfunc(T[i]-T0)
+        #if max(Ci) != 0:
+        #    # Solution for the initial value problem
+        #    for i in range(len(T)):
+        #        for j in range(len(Z)):
+        #            kesi = np.linspace(0,1,len(Ci))       
+        #            eqivpfunc = lambda Z, T: (np.exp(-(R*Z-R*kesi-T)**2/(4*T*R/P)) + np.exp(-P*kesi - (R*Z+R*kesi-T)**2/(4*T*R/P)))/(2*np.sqrt(np.pi*T/P/R)) - P/2*np.exp(P*Z)*erfc((R*Z+R*kesi+T)/(2*np.sqrt(T*R/P)))            
+        #            C1_ivp[j,i] = np.trapz(eqivpfunc(Z[j],T[i])*Ci,kesi)
     # SPA is kinetic
     elif spaflag == 1:
         m = 30 # number of modified bessel function terms used   
@@ -154,4 +156,5 @@ def analytical_soln(t,z,t0,pfas_tot,Ci,L,v,theta,rhob,D,Kd,alphas,Fs,R,Rs,Raw,sp
         C_tot = C1*R*theta + rhob*C2
     elif spaflag == 1:
         C_tot = C1*beta*R*theta + rhob*C2  
+
     return C1, C2, C_tot
