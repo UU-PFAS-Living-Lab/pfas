3c2b5d9ec80c55b710a46736e3cdffc54ac32b32
chStaiger <staigerchristine@gmail.com>
2025-10-23 13:19:58 +0200

    Create solvers.py

---
+++ src/pfas_leach_screening/solvers.py
@@ -0,0 +1,11 @@
+def kinetic_solver():
+  """
+  Kinetic solver and parameters.
+  """
+  print("TODO")
+
+def equilibrium_solver():
+  """
+  Equilibrium solver and its parameters.
+  """
+  print("TODO")
