d17d4e62ba8d360b64d77fc27d37209e837d5238
Valerie de Rijk <114667294+valeriederijk@users.noreply.github.com>
2026-02-05 15:53:44 +0100

    update with ruff

--- src/pfas/___init___.py
+++ src/pfas/___init___.py
@@ -5,8 +5,7 @@ Created on Thu Feb  3 14:26:22 2022.
 @author: boguo
 """
 
-from pfas.configuration import read_toml, validate_config
-
 from pfas.analytical_soln import analytical_soln
+from pfas.configuration import read_toml, validate_config
 
 __all__ = ["read_toml", "validate_config", "analytical_soln"]
